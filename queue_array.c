4c9353455d9f035f36a5cdbf9290b98a7d0f7ea9
betteenadenny <116335920+betteenadenny@users.noreply.github.com>
2022-11-01 14:40:06 +0530

    Add files via upload

---
+++ queue_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define s 3
+int q[s],r=-1,f=-1;
+void insert();
+void delete();
+void display();
+void main(){
+	intc; 	
+	while(1){
+	printf("\n Enter choice:");
+	printf("\n 1.Insert 2.delete 3.display 4.exit ");
+	scanf("%d"
