fd929dd67efd840c57156ee6094a310231565639
betteenadenny <116335920+betteenadenny@users.noreply.github.com>
2022-11-05 11:17:20 +0530

    Update queue_array.c

--- queue_array.c
+++ queue_array.c
@@ -1,13 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define s 3
+#define s 5
 int q[s],r=-1,f=-1;
 void insert();
 void delete();
 void display();
 void main(){
-	intc; 	
+	int c; 	
 	while(1){
+	printf("\n 1.Insert 2.delete 3.display 4.exit\n ");
 	printf("\n Enter choice:");
-	printf("\n 1.Insert 2.delete 3.display 4.exit ");
-	scanf("%d"
+	scanf("%d",&c);
+	switch(c){
+	    case 1:insert();
+	        break;
+        case 2:delete();
+	        break;
+        case 3:display();
+	        break;
+        case 4:exit(0);
+        default:printf("Wrong selection");
+	    }}
+	}
+	void insert(){
+	    if(r==(s-1))
+	        printf("overflow");
+        else{
+            if(f==-1)
+                f++;
+            int n;
+            printf("Enter elements:");
+            scanf("%d",&n);
+            r++;
+            q[r]=n;
+        }
+	}
+	void delete(){
+	    if(f==1||f>r)
+	        printf("Underflow");
+        else{
+            int t;
+            t=q[f];
+            printf("%d deleted",t);
+            f++;
+        }
+	}
+	void display(){
+	   if(f==-1||f>r)
+	        printf("Underflow no elements to print");
+        printf("Elements in queue:");
+        for(int i=f;i<=r;i++){
+            printf("\n%d",q[i]);
+        }
+	}
